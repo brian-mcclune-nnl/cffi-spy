73a2112668d18f78418d7085e1a064dcb6c3dfc3
Brian McClune <brian.mcclune@unnpp.gov>
2024-04-12 20:40:16 +0000

    Update profile_me.py to extend runtime

--- test/profile_me.py
+++ test/profile_me.py
@@ -1,4 +1,5 @@
 import argparse
+import math
 import sys
 from typing import List
 
@@ -14,21 +15,38 @@ def main(argv: List[str] = sys.argv[1:]):
     parser = argparse.ArgumentParser(description='Pi approximator')
     parser.add_argument(
         '-w', '--wallis', action='store_true',
-        help='use wallis product rather than leibniz formula')
+        help='use wallis product only')
+    parser.add_argument(
+        '-l', '--leibniz', action='store_true',
+        help='use leibniz formula only')
     parser.add_argument(
         'iterations', type=int, help='iterations count for formula or product')
 
     args = parser.parse_args(argv)
-    if args.wallis:
-        method = 'wallis'
-        approx_pi = lib.wallis(args.iterations)
-    else:
-        method = 'leibniz'
-        approx_pi = lib.leibniz(args.iterations)
+    if not args.wallis:
+        approx_pi('leibniz', args.iterations)
+    if not args.leibniz:
+        approx_pi('wallis', args.iterations)
 
+
+def approx_pi(method: str, iterations: int):
+    """Runs the approximator function specified by `method`.
+
+    Args:
+        method: The name of the method to use.
+        iterations: Max number of iterations to run.
+    """
+    approx_pi_func = getattr(lib, method)
+    for log_iteration in range(math.ceil(math.log10(iterations))):
+        iteration = 10 ** log_iteration
+        approx_pi = approx_pi_func(iteration)
+        print(
+            f'Approximate value of pi ({method} method, '
+            f'{iteration} iterations): {approx_pi}')
+    approx_pi = approx_pi_func(iterations)
     print(
         f'Approximate value of pi ({method} method, '
-        f'{args.iterations} iterations): {approx_pi}')
+        f'{iterations} iterations): {approx_pi}')
 
 
 if __name__ == '__main__':
